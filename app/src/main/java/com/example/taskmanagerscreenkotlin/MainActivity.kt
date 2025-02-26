3a2535f5c0ff85fb0d72162d0bb7ac4d7505ba95
Vinay Patel <vinayspatel0@gmail.com>
2025-02-26 13:12:40 +0000

    feat: import appropriate packages

--- app/src/main/java/com/example/taskmanagerscreenkotlin/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerscreenkotlin/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanagerscreenkotlin.ui.theme.TaskManagerScreenKotlinTheme
 
 class MainActivity : ComponentActivity() {
